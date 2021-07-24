c6d82a9e4d66340fa203999bd9bf4159e8c4e50b
Sreesaran-andalur <87901763+Sreesaran-andalur@users.noreply.github.com>
2021-07-24 18:29:32 +0530

    Update owner git.js

--- plugins/owner git.js
+++ plugins/owner git.js
@@ -10,7 +10,7 @@ if (Config.WORKTYPE == 'private') {
 
     var r_text = new Array ();
     
-    r_text[1] = "*╔═════😎Pikachu😎═════╗*\n           \n*⚜═Pikachu═⚜*\n\n*owner AmeerSuhail - http://Wa.me/+919946432377*\n* *\n🔰instagram:-https://www.instagram.com/ameer_.su_hail?r=nametag*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: AmeerSuhail*"
+    r_text[1] = "*╔═════💥ZREE HERE💥═════╗*\n           \n*⚜═ZREE HERE═⚜*\n\n*owner Sreesran-Andalur - http://Wa.me/+919400025258*\n* *\n🔰instagram:-https://www.instagram.com/sreesaranandalur?r=nametag*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: ZREE HERE*"
 
     
     await message.client.sendMessage(
@@ -23,7 +23,7 @@ if (Config.WORKTYPE == 'private') {
 
         var r_text = new Array ();
     
-        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner Ameer Suhail═💥*\n\n*💘https://github.com/ameer-kallumthodi/pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+        r_text[1] = "*Git links*\n           *\n💥═ZREE HERE Owner Sreesaran Andalur═💥*\n\n*💘https://github.com/Sreesaran-andalur/ZREE-HERE\n*     *⚜https://github.com/Sreesaran-andalur/ZREE-HERE*    
 
     
         await message.client.sendMessage(
@@ -39,7 +39,7 @@ if (Config.WORKTYPE == 'private') {
 
     var r_text = new Array ();
     
-    r_text[1] = "*╔═════😎Pikachu😎═════╗*\n           \n*⚜═Pikachu═⚜*\n\n*owner AmeerSuhail - http://Wa.me/+919946432377*\n* *\n🔰instagram:-https://www.instagram.com/ameer_.su_hail?r=nametag*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: AmeerSuhail*"
+    r_text[1] = "*╔═════💥ZREE HERE💥═════╗*\n           \n*⚜═ZREE HERE═⚜*\n\n*owner Sreesaran Andalur - http://Wa.me/+919400025258*\n* *\n🔰instagram:-https://www.instagram.com/sreesaranandalur?r=nametag*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: ZREE HERE*"
 
     
     await message.client.sendMessage(
@@ -51,8 +51,8 @@ if (Config.WORKTYPE == 'private') {
         asena.addCommand({pattern: 'git', fromMe: false, deleteCommand: true, desc: GIT,}, (async (message, match) => {
 
         var r_text = new Array ();
-    
-        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner Ameer Suhail═💥*\n\n*💘https://github.com/ameer-kallumthodi/pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+
+        r_text[1] = "*Git links*\n           *\n💥═ZREE HERE Owner Sreesaran Andalur═💥💘
 
     
         await message.client.sendMessage(
